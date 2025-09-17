7bb58ce7cf3d26ed44db36563a9ac7bc4883427f
[EXT] Dakshitha Thiriveedhi <condthi@partner.waters.com>
2025-09-17 15:55:36 +0530

    added the dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [{
-    path: 'login',
-    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+     path: 'login',
+     loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+     path: 'admin/dashboard',
+     loadComponent: () => import('./admin/dashboard/dashboard.component').then(m => m.DashboardComponent)
   },
   {
     path: '',
