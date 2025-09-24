9fcc33bc4cdccde569cb5be57c7e62799af15383
saqib <realhussainsakib@gmail.com>
2025-09-24 06:54:16 +0000

    new way of creating passwords

--- CreatingPasswords/Program.cs
+++ CreatingPasswords/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using System.Security.Cryptography;
 
 class Program
 {
     public static void HashAndDisplay(string password)
     {
+        Console.WriteLine("Using Bcrypt library");
         // Check if the input string is null or empty
         if (string.IsNullOrEmpty(password))
         {
@@ -15,7 +16,7 @@ class Program
 
         // Hash the password. BCrypt.Net handles salt generation automatically.
         string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
-        
+
         Console.WriteLine($"Hashed String: {hashedPassword}");
         Console.WriteLine("---");
 
@@ -23,18 +24,40 @@ class Program
         bool isVerified = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
         Console.WriteLine($"Verification check: {(isVerified ? "Successful" : "Failed")}");
     }
+    private const int SaltSize = 16;
+    private const int KeySize = 32;
+    private const int Iterations = 10000;
+    private const char Delimiter = ';';
+    private static readonly HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA256;
+
+    public static string HashInbuilt(string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(SaltSize);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithm, KeySize);
+        return string.Join(Delimiter, Convert.ToBase64String(salt), Convert.ToBase64String(hash));
+    }
+    public static bool HashVerify(string password, string hashedPassword)
+    {
+        var parts = hashedPassword.Split(Delimiter);
+        if (parts.Length != 2)
+        {
+            return false;
+        }
+
+        var salt = Convert.FromBase64String(parts[0]);
+        var hash = Convert.FromBase64String(parts[1]);
+
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithm, KeySize);
+
+        return hashToCompare.SequenceEqual(hash);
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("--- BCrypt Password Hashing ---");
-        
+        Console.WriteLine("--- Inbuilt Password Hashing ---");
+
         // Call the function with a sample password
-        HashAndDisplay("hlo123");
-
-        // Call it again with a different password
-        HashAndDisplay("hii12345");
-
-	HashAndDisplay("12345");
-
-	HashAndDisplay("1234");
+        string hasheg = HashInbuilt("hlo123");
+        Console.WriteLine(hasheg);
+        Console.WriteLine(HashVerify("hlo123", hasheg));
     }
 }
