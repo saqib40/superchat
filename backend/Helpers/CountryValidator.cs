0beb7ffedccf71a615514c8b4fd71e844bbc25cc
[EXT] Dakshitha Thiriveedhi <condthi@partner.waters.com>
2025-10-10 12:04:06 +0530

    added country enum validation

---
+++ backend/Helpers/CountryValidator.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models;
+
+namespace backend.Helpers
+{
+    public static class CountryValidator
+    {
+        public static bool IsValidCountry(string country)
+        {
+            return Enum.TryParse(typeof(Country), country.Replace(" ", ""), ignoreCase: true, out _);
+        }
+    }
+}
+
